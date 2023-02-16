83375e58c00b1306a4b25d344d77e24a6ff8012a
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-17 00:39:26 +0530

    Create 01_variable_assign.js

---
+++ 01_variable_assign.js
@@ -0,0 +1,9 @@
+var myFullName;
+console.log(myFullName);
+myFullName="Pooja Rupanwar";
+console.log(myFullName);
+myFullName="Pooja Madhukar rupanwar";
+console.log(myFullName);
+var myLove="I love only JS"
+console.log(myLove);
+
