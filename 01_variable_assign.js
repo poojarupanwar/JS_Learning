284ba5c73309b27866d8f9c2e7b649380bc0ad5b
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-17 02:09:29 +0530

    Updated file

--- 01_variable_assign.js
+++ 01_variable_assign.js
@@ -1,9 +1,19 @@
+console.log("**************** Step 1 **********************\n")
 var myFullName;
-console.log(myFullName);
 myFullName="Pooja Rupanwar";
-console.log(myFullName);
-myFullName="Pooja Madhukar rupanwar";
-console.log(myFullName);
+console.log("My fullName is :",myFullName);
+myFullName="Pooja Madhukar Rupanwar";
+console.log("My fullName is :",myFullName);
+console.log("\n**************** Step 2 **********************\n")
 var myLove="I love only JS"
-console.log(myLove);
-
+console.log("My Love:",myLove);
+console.log("\n**************** Step 3 **********************\n")
+var firstName="Pooja";
+var lastName="Rupanwar";
+var age=22;
+var address="Rahatani Pune";
+var pincode=411017;
+var gender="female";
+var isMarried="No";
+console.log("My First Name is:",firstName,"Last name is : "+lastName+" Age :"+age+" Address :"+address+" pincode :"+pincode+" Gender :"+gender+" Married :"+isMarried);
+ 
