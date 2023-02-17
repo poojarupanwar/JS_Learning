a8fb5f24698da2c4f7add851eb14b9ad43eb914a
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-18 00:29:04 +0530

    Create 01_variableBasicsAssign.js
    New file created with data in it

---
+++ 01_variableBasicsAssign.js
@@ -0,0 +1,20 @@
+console.log("******************** Step 1 **********************\n");
+var num=12;
+num=34;
+var numType=typeof num;
+console.log(num," :",numType);
+var str="pooja";
+var strType=typeof str;
+console.log("string:",str,"type:",strType);
+var booleanVal=true;
+var boolType=typeof booleanVal;
+console.log("booleanVal:"+booleanVal,",","type:",boolType);
+var obj=Object(6);
+var objType=typeof obj;
+console.log("Object :",obj,"Type :",objType);
+console.log("\n********************** Step 2 *********************");
+var nullVal=null;
+var nullValType=typeof nullVal;
+console.log("nullVal:"+nullVal+"Type of",nullValType);
+
+
