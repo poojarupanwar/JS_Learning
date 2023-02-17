4a37400e0cd5915634fb33f4d9667cec48d36279
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-18 00:30:22 +0530

    Create 02_typeOf.js
    New file created with values and datatype using typeOf operator.
    declare variable
    initialize
    and log

---
+++ 02_typeOf.js
@@ -0,0 +1,26 @@
+console.log("****************** step 1 **************");
+var bankName;
+console.log(bankName);
+bankName="SBI Bank";
+var bankType=typeof bankName;
+console.log("Bank name:",bankName,",type:",bankType);
+console.log("****************** step 2 **************");
+var marks="90%";
+var marksType=typeof marks;
+console.log("marks",marks,",Marks type:",marksType);
+
+console.log("****************** step 3 **************");
+var isWorking;
+console.log(isWorking);
+isWorking=true;
+console.log(isWorking);
+var isWorkingType=typeof isWorking;
+console.log("is working",isWorking,",Type:",isWorkingType);
+
+console.log("********************** step 4 **************");
+var totalCount="Hundred and seven";
+console.log("Total Count",totalCount,",Type:",typeof(totalCount));
+totalCount=107;
+console.log("Total Count",totalCount,",Type:",typeof(totalCount));
+
+
