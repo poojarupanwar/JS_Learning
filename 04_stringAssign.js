b51e29f64ed683435ccca81c41b208b46ce3a270
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-21 22:41:06 +0530

    04_string_Assign.js
    function added with original string

---
+++ 04_stringAssign.js
@@ -0,0 +1,8 @@
+
+console.log("************************ 1 ****************************")
+
+function stringHandsOn(){
+    var str="   Hey you are doing good,keep it up    ";
+    console.log("Original String:",str);
+}
+stringHandsOn();
