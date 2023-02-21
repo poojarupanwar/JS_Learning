1f21478b1cd493f1125c9d88d1b3aa35aab18277
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-21 22:46:55 +0530

    Update 04_stringAssign.js
    string length added
    removed leading and trailing space from string

--- 04_stringAssign.js
+++ 04_stringAssign.js
@@ -1,8 +1,13 @@
-
-console.log("************************ 1 ****************************")
-
 function stringHandsOn(){
+    console.log("************************ 1 ****************************")
     var str="   Hey you are doing good,keep it up    ";
     console.log("Original String:",str);
+
+    console.log("************************ 2 ****************************")
+    console.log("Total Length of string:",str.length);
+
+    console.log("************************ 3 ****************************")
+    console.log("String after trim",str.trim);
+
 }
 stringHandsOn();
