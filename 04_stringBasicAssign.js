1375460dbcfb0a9e2f5a9a0bc68ad60a2062584d
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-21 22:40:19 +0530

    04_stringBasicsAssign
    content added to file

---
+++ 04_stringBasicAssign.js
@@ -0,0 +1,13 @@
+console.log("***************************  1  ************************")
+function stringBasics(){
+    console.log("My dream company is Amazon");
+}
+console.log("***************************  2.1  ************************")
+
+var hobby1="Dancing";
+var hobby2="Singing";
+var hobby3="Gardening";
+console.log("My hobbies are :","1",hobby1,"2",hobby2,"3",hobby3);
+console.log("***************************  2.2  ************************")
+
+console.log("Total Number of characters in Hobby are:",hobby1.length+hobby2.length+hobby3.length);
