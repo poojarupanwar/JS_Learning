0772613e57dea3a41c649bdac39a7c0d3e72e03d
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-22 15:44:25 +0530

    Update operator_Assi.js

---
+++ 05_operatorAssi.js
@@ -0,0 +1,15 @@
+function squareOfWordLength(value){
+   var lenVal=value.length*value.length;
+console.log(`Given String is :${value}`);
+console.log(`Total Length of string ${value} is ${value.length}`);
+console.log(lenVal);
+}
+console.log("----------------------------Given String Length And its Square-------------------------------")
+
+console.log("----------------------------   1  -------------------------------")
+
+squareOfWordLength("Javascript");
+console.log("----------------------------   2  -------------------------------")
+squareOfWordLength("Google Crome");
+console.log("----------------------------   3  -------------------------------")
+squareOfWordLength("Developer smart");
