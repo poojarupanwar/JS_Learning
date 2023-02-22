4c5562d2ec97ffff72ec63ebf52164d6d913dfb9
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-22 20:03:26 +0530

    Update operator_Assi.js
    Updated file

--- 05_operatorAssi.js
+++ 05_operatorAssi.js
@@ -8,8 +8,23 @@ console.log("----------------------------Given String Length And its Square-----
 
 console.log("----------------------------   1  -------------------------------")
 
-squareOfWordLength("Javascript");
+squareOfWordLength("JavaScript");
 console.log("----------------------------   2  -------------------------------")
-squareOfWordLength("Google Crome");
+squareOfWordLength("Google Chrome");
 console.log("----------------------------   3  -------------------------------")
-squareOfWordLength("Developer smart");
+squareOfWordLength("Developer Smart");
+
+console.log("----------------------------Function with no return type and no return value-------------------------------")
+
+function developer()
+{
+    var str="I am Angular Developer";
+    var strLen=str.length;
+    var totalWordsStr=str.split(" ");
+    var totalWLen=totalWordsStr.length;
+    console.log(`The String is:${str} ,Length of String is : ${strLen}`);
+    console.log(`The Total Words of String are:${totalWordsStr} ,Length of total worlds is : ${totalWLen}`);
+    console.log(`The result of string length divided by total number of words is :${strLen/totalWLen}`);
+    console.log(`The result of string length multiplication by total number of words is:${strLen*totalWLen}`);
+}
+developer();
