bb6e57591af593129dfc0bbd907fa3ccb94c66d8
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-22 15:11:53 +0530

    operator.js
    file created and learnedsome features of operator

---
+++ 05_operator.js
@@ -0,0 +1,35 @@
+console.log(`======== Arithmetic Operator ==========`);
+var num1=10;
+var num2=2;
+var result=num1+num2;
+console.log(`Addition is ${result}`);
+
+result=num1-num2;
+console.log(`Substraction is ${result}`);
+
+result=num1*num2;
+console.log(`Multiplication  is ${result}`);
+
+result=num1**num2;
+console.log(`Exponential is ${result}`);
+
+result=num1/3;
+console.log(`Division is ${result}`);
+
+result=num1%num2;
+console.log(`Remainder is ${result}`);
+
+//compound addition.
+var num=10;
+num+=20;
+console.log(num);
+var num3=10;
+console.log(num3==10);
+
+console.log(`conversion operator :`);
+var num3="10";
+var num4=10;
+console.log(num3==num4);
+
+console.log(`strict equity operator :`);
+console.log(num3===num4);
