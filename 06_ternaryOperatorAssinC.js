bdb74930a8876c46e1b32ae9bada6a6bdc7726bc
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-23 14:00:52 +0530

    file created ternaryOperatorC.js

--- 06_ternaryOperatorAssinC.js
+++ 06_ternaryOperatorAssinC.js
@@ -1,6 +1,9 @@
 //Use of Ternry operator and function expression
-
+console.log("\n--------------------------------------Use of Ternary operator and function expression----------------\n");
 function expression(gradScore,hscScore,sscScore,candidateName){
 var eligible=gradScore>=70 || hscScore>=80 || sscScore>=90 ? `Congrats ${candidateName} you are eligible for TCS interview` :`Unfortunately you are not eligible for the Interview`
+console.log(eligible);
 }
-expression()
+expression(80,86,90,"Pooja");
+expression(70,65,55,"kirti");
+expression(60,79,88,"Reeya");
