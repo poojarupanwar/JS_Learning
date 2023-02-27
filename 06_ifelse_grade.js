8e6ec6c8fddbcfcabf58e168f507fa529972b214
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-02-28 01:56:14 +0530

    format document

--- 06_ifelse_grade.js
+++ 06_ifelse_grade.js
@@ -1,15 +1,14 @@
 console.log("---------------------------- step 1 -------------")
-function voteEligibility(age){
-if(age<0 || age==undefined || age>100){
-   console.log(`This is not valid age : '${age}' please enter valid age`);
+function voteEligibility(age) {
+    if (age < 0 || age == undefined || age > 100) {
+        console.log(`This is not valid age : '${age}' please enter valid age`);
     }
-else if(age>=18)
-{
-    console.log(`Your age is : '${age}' you are Eligible`);
-}
-else{
-    console.log(`Your age is : '${age}' you are Not-Eligible`);
-}
+    else if (age >= 18) {
+        console.log(`Your age is : '${age}' you are Eligible`);
+    }
+    else {
+        console.log(`Your age is : '${age}' you are Not-Eligible`);
+    }
 }
 voteEligibility(45);
 voteEligibility(17);
@@ -23,27 +22,22 @@ voteEligibility(null);
 
 console.log("--------------------- step 2 -----------------");
 
-function gradeCalulation(marks){
-if(marks>=90)
-{
-    console.log(`Fantastic Marks : ${marks} ,Your grade is A+`)
-}
-else if(marks>=75)
-{
-    console.log(`Excellent Marks : ${marks} ,Your grade is A`)
-}
-else if(marks>=50 && marks<75)
-{
-    console.log(`Good Marks : ${marks} ,Your grade is B`)
-}
-else if(marks>=35 && marks<50)
-{
-    console.log(`Marks is : ${marks} ,Your grade is C ,Need Improvment`)
-}
-else if(marks<=0 || marks>100 || marks!=Number || marks===String)
-{
-    console.log(`Please provide valid marks -${marks}`)
-}
+function gradeCalulation(marks) {
+    if (marks >= 90) {
+        console.log(`Fantastic Marks : ${marks} ,Your grade is A+`)
+    }
+    else if (marks >= 75) {
+        console.log(`Excellent Marks : ${marks} ,Your grade is A`)
+    }
+    else if (marks >= 50 && marks < 75) {
+        console.log(`Good Marks : ${marks} ,Your grade is B`)
+    }
+    else if (marks >= 35 && marks < 50) {
+        console.log(`Marks is : ${marks} ,Your grade is C ,Need Improvment`)
+    }
+    else if (marks <= 0 || marks > 100 || marks != Number || marks === String) {
+        console.log(`Please provide valid marks -${marks}`)
+    }
 }
 gradeCalulation(98)
 gradeCalulation(80)
