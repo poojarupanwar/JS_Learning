0691eb2c4ef48dba6828bba5b90c1b9d502ec142
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-03-03 15:39:31 +0530

    Create Assignment0B.js

---
+++ Assignment0B.js
@@ -0,0 +1,28 @@
+function check_leap_year(leapYear){
+     
+     if((leapYear%4==0) || (leapYear%100===0) || (leapYear%200===0 ) ){
+        console.log(`Year ${leapYear} is aleap year`);
+     }
+     else if(leapYear!=Number){
+        console.log(` '${leapYear}' is not a valid Year, Please provide valid leap year.`)
+     }
+    else{
+        console.log("Not a leap year")
+    }
+    
+    
+    
+}
+check_leap_year(2020);
+check_leap_year(1999);
+check_leap_year(1600);
+check_leap_year(2022);
+check_leap_year(1945);
+check_leap_year(null);
+check_leap_year("Twenty Twenty");
+check_leap_year(undefined);
+check_leap_year(NaN);
+check_leap_year(1750);
+
+
+
