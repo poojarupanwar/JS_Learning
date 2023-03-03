b6b76dee8c6bf6f6998f22d707b7a6e14c6144b2
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-03-04 00:41:25 +0530

    create Assignment 06_for_Assignment01.js

---
+++ 06_for_Assignment01.js
@@ -0,0 +1,20 @@
+//Print Numbers from 5 t0 15
+console.log("Print Numbers from 5 t0 15\n");
+for(var i=5;i<=15;i++)
+{
+    console.log(i);
+}
+console.log("\nPrint Numbers from 50 t0 40 in decrement order");
+
+for(var i=50;i>=40;i--)
+{
+    console.log(i);
+}
+console.log("\n print first 15 odd numbers");
+
+for(var i=0;i<=15;i++)
+{
+    if(i%2!=0){
+        console.log(`Value ${i} is odd`)
+    }
+}
