3a2b159bbcd8e1c88a3450f3f6cef134d29ea82d
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-03-03 05:44:33 +0530

    Create Assignment0A.js

---
+++ Assignment0A.js
@@ -0,0 +1,48 @@
+function monthOfYear(monthNumber){
+    switch(monthNumber){
+        case 1:
+            console.log("You have entered January");
+            break;
+        case 2:
+            console.log("You have entered Februry");
+            break;
+        case 3:
+            console.log("You have entered March");
+            break;
+        case 4:
+            console.log("You have entered April");
+            break;
+        case 5:
+            console.log("You have entered May");
+            break;
+        case 6:
+            console.log("You have entered June");
+            break;
+        case 7:
+            console.log("You have entered July");
+            break;
+        case 8:
+            console.log("You have entered August");
+            break;
+        case 9:
+            console.log("You have entered Supteber");
+            break;
+        case 10:
+            console.log("You have entered October");
+            break;
+        case 11:
+            console.log("You have entered November");
+            break;
+        case 12:
+            console.log("You have entered December");
+            break;
+        default:
+            console.log("Invalid month of year");
+    }
+}
+monthOfYear()
+monthOfYear(2)
+monthOfYear(5)
+monthOfYear(12)
+monthOfYear(15)
+monthOfYear(100)
