d5c707609d4901da537dab74d8694ff93b45b5c0
Pooja Rupanwar <pooja.rupanwar5767@gmail.com>
2023-03-04 19:42:43 +0530

    update 06_for_Assignment01.js

--- 06_for_Assignment01.js
+++ 06_for_Assignment01.js
@@ -18,3 +18,43 @@ for(var i=0;i<=15;i++)
         console.log(`Value ${i} is odd`)
     }
 }
+
+console.log("\n print first 10 even numbers");
+
+for(var i=0;i<=10;i++)
+{
+    if(i%2==0){
+        console.log(`Value ${i} is Even`)
+    }
+}
+console.log("\n Write a program to print table of 5");
+
+for(var i=0;i<=15;i++)
+{
+    if(i%2!=0){
+        console.log(`Value ${i} is odd`)
+    }
+}
+
+console.log("\n print table of 5");
+
+for(var i=1;i<=10;i++)
+{
+    console.log(i*5)
+    
+}
+console.log("\n print table of 10");
+
+for(var i=1;i<=10;i++)
+{
+    console.log(i*10)
+    
+}
+
+console.log("\n print table of 10 in reverse");
+
+for(var i=10;i>=0;i--)
+{
+    console.log(i*10)
+    
+}
