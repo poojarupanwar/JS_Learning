00999b57cf34d267c3ba8453380579bd3cb8e01d
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-03-13 11:34:19 +0530

    create 10_constructor_fun_assign.js

---
+++ 10_constructor_fun_assign.js
@@ -0,0 +1,8 @@
+function Bank(bankName,accountNo,location,ifsc){
+    this.bankName=bankName;
+
+}
+const yesBank=new Bank("");
+const sbiBank=new Bank("");
+const mahBank=new Bank("");
+const axisBank=new Bank("");
