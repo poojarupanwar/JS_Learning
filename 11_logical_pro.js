4402383575d92167f9ea8b8ef094283855b45005
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-03-16 19:42:05 +0530

    Create logical pro file

---
+++ 11_logical_pro.js
@@ -0,0 +1,14 @@
+console.log(`---------  Fibonaci series ---------------`)
+let n1=0;
+let n2=1;
+fibo=1
+for(let i=0;i<10;i++){
+console.log(fibo);
+fibo=n1+n2;
+n1=n2;
+n2=fibo;
+}
+console.log(`---------  Factorial ---------------`)
+
+console.log(`---------  Prime Nos ---------------`)
+
