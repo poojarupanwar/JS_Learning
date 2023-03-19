12fb94964018f080f2f2ea7f930c0afe2d7f371d
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-03-19 13:12:59 +0530

    update 11_logical_pros.js

--- 11_logical_pro.js
+++ 11_logical_pro.js
@@ -1,14 +1,32 @@
 console.log(`---------  Fibonaci series ---------------`)
-let n1=0;
-let n2=1;
-fibo=1
-for(let i=0;i<10;i++){
-console.log(fibo);
-fibo=n1+n2;
-n1=n2;
-n2=fibo;
+// 0  1  1   2   3   5   8
+
+function fabSeries(fabTerm){
+    let first = 0;
+    let second = 1;
+    let next = 0;
+    for (let index = 1; index <= fabTerm; index++) {
+     console.log(first);   
+      next = first + second; 
+      first = second;
+      second = next;  
+    }
 }
+fabSeries(7);
+
 console.log(`---------  Factorial ---------------`)
+
 
 console.log(`---------  Prime Nos ---------------`)
 
+let n=20;
+for(let i=1;i<=n;i++){
+    
+    if(n%i==0)
+    {
+        console.log(i+"it is not prime no")
+    }
+    else{
+        console.log(i+ "it is prime no")
+    }
+}
