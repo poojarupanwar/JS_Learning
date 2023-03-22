d5237eea867f6f39bb0423c2c0fd26a8dddc0982
Pooja Rupanwar <46537033+poojarupanwar@users.noreply.github.com>
2023-03-22 23:55:40 +0530

    Update demo.js

--- demo.js
+++ demo.js
@@ -1,1 +1,16 @@
- 
+//  let student={
+//     name:"pooja",
+//     age:23,
+//     city:"pune",
+//     company:"avalara"
+//  }
+//  for(let i in student){
+//     console.log(student[i])
+//  }
+
+ let arr=["pooja","kirti","reeya"];
+ for(let i of arr){
+    if(i[0]=="p"){
+        console.log(`this is right ${i}`);
+    }
+ }
